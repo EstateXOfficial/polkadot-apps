5fecedba0ac88d48b4682d85f47bd86d9a562d51
Alina Radyukevich <a.radyukevich@pixelplex.io>
2025-08-05 13:56:56 +0300

    [ESTD-5012, ESTD-5013]: add layout dropdown + search block

---
+++ packages/page-dashboard/src/components/Query/index.tsx
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import React, { memo } from 'react';
+import Query from './Query.tsx';
+
+const QueryBlock = () => {
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <Query />
+    </div>
+  );
+};
+
+export default memo(QueryBlock);
