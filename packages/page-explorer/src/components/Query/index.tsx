c25ec300a7ca751215b1668eabd175641126f2dd
s.lobikov <s.lobikov@pixelplex.io>
2025-08-11 15:04:12 +0300

    [ESX-10]: add gif

--- packages/page-explorer/src/components/Query/index.tsx
+++ packages/page-explorer/src/components/Query/index.tsx
@@ -1,13 +1,34 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React, { memo } from 'react';
+import { styled } from '@polkadot/react-components';
 import Query from './Query.js';
+
+const StyledQueryContainer = styled.div`
+  margin-bottom: 10px;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 20px;
+  align-items: center;
+
+  .dashboardGif {
+    width: 100%;
+    border-radius: 6px;
+  }
+
+  @media screen and (min-width: 1441px) {
+    grid-template-columns: 3fr 1fr;
+  }
+`;
 
 const QueryBlock = () => {
   return (
-    <div style={{ marginBottom: 10 }}>
-      <Query />
-    </div>
+    <StyledQueryContainer>
+      <Query/>
+      <div>
+        <img className={'dashboardGif'} src="/assets/estate.gif" alt="dashboard gif"/>
+      </div>
+    </StyledQueryContainer>
   );
 };
 
